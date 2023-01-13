2569e8a4a2f0ea528822cf1236d052967f3383df
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2023-01-13 19:00:46 +0200

    filterByNDZ

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
 const express = require('express')
-const axios = require("axios")
-const { XMLParser } = require("fast-xml-parser")
+const { scrapeDrones } = require('./services/scrapeDrones')
+const { parseDrones } = require('./services/parseDrones')
+const { filterByNDZ } = require('./services/filterByNDZ')
 
 const app = express()
-
-const scrapeDrones = async () => {
-    const dronesUrl = 'https://assignments.reaktor.com/birdnest/drones'
-    
-    const request = axios.get(dronesUrl)
-    const response = await request
-
-    return response.data
-}
-
-const parseDrones = async (data) => {
-    const parser = new XMLParser()
-    const parsedData = parser.parse(data)
-                        .report.capture.drone
-
-    const allDrones = parsedData
-                        .map(drone => ({
-                            serialNumber: drone.serialNumber,
-                            Y: parseFloat(drone.positionY),
-                            X: parseFloat(drone.positionX)
-                        }))
-
-    return allDrones
-}
 
 app.get('/api/drones', async (req, res) => {
     const data = await scrapeDrones()
     const drones = await parseDrones(data)
+    const ndzDrones = await filterByNDZ(drones)
 
     console.log('')
     console.log('this is the raw data:')
@@ -43,6 +21,10 @@ app.get('/api/drones', async (req, res) => {
     console.log(drones)
     console.log('')
 
+    console.log('these are the ndz drones:')
+    console.log('')
+    console.log(ndzDrones)
+    console.log('')
 
     res.send('hello world!')
 })
