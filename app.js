5a1cbc42927d8247ef98d7cf96995340e2807646
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2023-01-17 02:01:18 +0200

    Add PORT env variable

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const config = require('./utils/config')
 const WebSocket = require('ws')
+const PORT = process.env.PORT || 3001
 
 const { connectDb } = require('./lib/connectDb')
 const { updateDatabase } = require('./src/updateDatabase')
@@ -8,7 +9,7 @@ const getAllFromDb = require('./lib/getAllFromDb')
 connectDb(config.MONGODB_URI)
 setInterval(updateDatabase, 2000)
 
-const wss = new WebSocket.Server({ port: 3001 }) 
+const wss = new WebSocket.Server({ PORT }) 
 wss.on('connection', (ws) => {
   getAllFromDb().then(data => ws.send(JSON.stringify(data)))
 
