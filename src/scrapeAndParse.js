416d2c11793b835800385a4bc1d8993cd764673b
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2023-01-19 10:23:06 +0200

    Add logic for catching drone scraping errors

--- src/scrapeAndParse.js
+++ src/scrapeAndParse.js
@@ -5,6 +5,9 @@ const { getNDZPilots } = require('../lib/getNDZPilots')
 
 const scrapeAndParse = async () => {
     const data = await scrapeDrones()
+
+    if (!data) return undefined
+    
     const drones = await parseDrones(data)
     const ndzDrones = await filterByNDZ(drones)
     const ndzPilots = await getNDZPilots(ndzDrones)
